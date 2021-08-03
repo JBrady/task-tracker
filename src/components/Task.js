c9e77d7684ff8eba052c115fe55c53dd2e1cd59c
John <jbradysd@icloud.com>
2021-08-02 18:53:18 -0700

    renaming task prop to singleTask for clarity

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,5 +1,10 @@
-const Task = () => {
-  return <div></div>
+const Task = ({ singleTask }) => {
+  return (
+    <div className='task'>
+      <h3>{singleTask.text}</h3>
+      <p>{singleTask.day}</p>
+    </div>
+  )
 }
 
 export default Task
